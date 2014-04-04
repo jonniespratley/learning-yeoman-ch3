c29de0ab416a119573ff642268663790173237e7
Jonnie Spratley <jonniespratley@gmail.com>
2014-04-03 23:44:35 -0700

    updated karama

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,12 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             'app/bower_components/angular/angular.js',
-            'app/bower_components/angular/angular-mocks.js',
+            'app/bower_components/angular-cookies/angular-cookies.js',
+            'app/bower_components/angular-resource/angular-resource.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-sanitize/angular-sanitize.js',
+            'app/bower_components/angular-scenario/angular-scenario.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
             '.tmp/scripts/**/*.js',
             '.tmp/spec/**/*.js'
 
@@ -41,7 +46,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
 
 
         // Continuous Integration mode
