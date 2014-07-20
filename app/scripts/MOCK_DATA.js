48775c87dd3630af4a9bff2b49d4190afd6aa93a
Jonnie Spratley <jonniespratley@me.com>
2014-07-20 15:53:22 -0700

    added mock posts

---
+++ app/scripts/MOCK_DATA.js
@@ -0,0 +1,37 @@
+window.MOCK_POSTS = [
+  {
+    "body": "PhantomJS is a headless WebKit scriptable with a JavaScript API. It has fast and native support for various web standards: DOM handling, CSS selector, JSON, Canvas, and SVG.",
+    "image": "https://dl.dropboxusercontent.com/u/26906414/angular-cms/images/feature-phantomjs.png",
+    "published": true,
+    "slug": "phantomjs",
+    "title": "PhantomJS",
+    "_id": "530575dfe144fc4271000065"
+  },
+  {
+    "title": "Jasmine",
+    "slug": "jasmine",
+    "body": "Jasmine is a Behavior Driven Development testing framework for JavaScript. It does not rely on browsers, DOM, or any JavaScript framework. ",
+    "image": "http://a481ab4f6ea4dd65cff0-b2b68ced242ecf1cb9bc1021688e3775.r49.cf1.rackcdn.com/angular-cms/images/feature-jasmine.png",
+    "published": true,
+    "tags": "generator-angular, generator-backbone, generator-ember, generator-webapp",
+    "_id": "533052fdca0f98da7ebcf3e8"
+  },
+  {
+    "title": "CoffeeScript",
+    "slug": "coffeescript",
+    "body": "CoffeeScript is a little language that compiles into JavaScript. CoffeeScript is an attempt to expose the good parts of JavaScript in a simple way.",
+    "image": "http://a481ab4f6ea4dd65cff0-b2b68ced242ecf1cb9bc1021688e3775.r49.cf1.rackcdn.com/angular-cms/images/feature-coffeescript.png",
+    "published": true,
+    "tags": "generator-angular, generator-backbone, generator-ember, generator-webapp",
+    "_id": "533053cfca0f98da7ebcf3e9"
+  },
+  {
+    "title": "AngularJS",
+    "slug": "angularjs",
+    "body": "AngularJS is a toolset for building the framework most suited to your application development. It is fully extensible and works well with other libraries. ",
+    "image": "http://a481ab4f6ea4dd65cff0-b2b68ced242ecf1cb9bc1021688e3775.r49.cf1.rackcdn.com/angular-cms/images/feature-angular.png",
+    "published": true,
+    "tags": "generator-angular, generator-backbone, generator-ember, generator-webapp",
+    "_id": "53305410ca0f98da7ebcf3ea"
+  }
+];
